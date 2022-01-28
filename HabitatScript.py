f5d097d3913034ddceca5de96d455235309362b8
Luis <yomiguel112@gmail.com>
2022-01-27 21:05:37 -0500

    script

---
+++ HabitatScript.py
@@ -0,0 +1,6 @@
+﻿# coding: utf-8
+import arcpy
+from arcpy.sa import *
+baseRaster = Raster('DEM_elkhorn.tif')
+falconHabitat = (Slope(baseRaster, 'DEGREE') > 40) & ((baseRaster-baseRaster.mean) > 160)
+falconHabitat.save('C:\\PythonScriptsRaster\\Data\\HabitatOutput.tif')
